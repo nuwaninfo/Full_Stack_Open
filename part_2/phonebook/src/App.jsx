1f80e33587886224177b2a4d5b8ceafab66d9dc2
Nuwan Fernando <nuwaninfo@gmail.com>
2023-12-21 15:12:02 +0200

    Completion of 2.6: The Phonebook Step 1

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -4,19 +4,56 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    /*let newPersons = [];
+    
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    // Get name as object from namr input field
+    const objFRomInput = Object.fromEntries(formData.entries());
+
+    // Check the name being added is already in the array
+    const isEqual = persons.some(
+      (item) => JSON.stringify(item) === JSON.stringify(objFRomInput)
+    );
+    if (isEqual === false) {
+      console.log("hhhhh");
+      newPersons = [...persons, objFRomInput];
+    } else {
+      alert(`${newName}`);
+    }
+
+    setPersons(newPersons);*/
+
+    const newPersons = [...persons, { name: newName }];
+    setPersons(newPersons);
+    console.log(persons);
+  };
+
+  // Get the value of input field
+  const handleName = (event) => {
+    setNewName(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form method="post" onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
+      {persons.map((person) => {
+        return <div>{person.name}</div>;
+      })}
     </div>
   );
 };
