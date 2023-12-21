ba9d1e55fa03b5a29c97188a8f50bdbcb52ae2bb
Nuwan Fernando <nuwaninfo@gmail.com>
2023-12-21 18:50:37 +0200

    Completion of exercise 2.8: The Phonebook Step 3

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,30 +1,39 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     let newPersons = [];
 
-    const newNameObj = { name: newName };
+    const newObj = { name: newName, number: newNumber };
 
     // Check the name being added is already in the array
     const isEqual = persons.some(
-      (item) => JSON.stringify(item) === JSON.stringify(newNameObj)
+      (item) => JSON.stringify(item) === JSON.stringify(newObj)
     );
     if (isEqual === false) {
-      newPersons = [...persons, newNameObj];
+      newPersons = [...persons, newObj];
+
       setPersons(newPersons);
     } else {
       alert(`${newName} is already added to phonebook`);
     }
   };
 
-  // Get the value of input field
+  // Get the value of name
   const handleName = (event) => {
     setNewName(event.target.value);
+  };
+
+  // Get the value of input number
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -35,12 +44,19 @@ const App = () => {
           name: <input value={newName} onChange={handleName} />
         </div>
         <div>
+          Numbers: <input value={newNumber} onChange={handleNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => {
-        return <div key={person.name}>{person.name}</div>;
+        return (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        );
       })}
     </div>
   );
