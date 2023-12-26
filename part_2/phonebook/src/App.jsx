bab14aee1940b551134c6aeeaae773c0774f7bdc
Nuwan Fernando <nuwaninfo@gmail.com>
2023-12-26 23:38:53 +0200

    Completion of exercise 2.9*: The Phonebook Step 4

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 
+let nextId = 0;
+
 const App = () => {
+  let filteredPerson = [];
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-1234567", id: nextId },
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState([]);
+  const [serchResults, setSearchResults] = useState(persons);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     let newPersons = [];
 
-    const newObj = { name: newName, number: newNumber };
+    const newObj = { name: newName, number: newNumber, id: nextId + 1 };
 
     // Check the name being added is already in the array
     const isEqual = persons.some(
@@ -20,10 +26,17 @@ const App = () => {
     if (isEqual === false) {
       newPersons = [...persons, newObj];
 
+      setSearchResults(newPersons);
       setPersons(newPersons);
     } else {
       alert(`${newName} is already added to phonebook`);
     }
+
+    // Clear text fields
+    setNewName("");
+    setNewNumber("");
+
+    nextId = nextId + 1;
   };
 
   // Get the value of name
@@ -36,9 +49,25 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  // Fliter name in case insensitive
+  const handleFilter = (event) => {
+    const inputName = event.target.value;
+    setFilterName(inputName);
+
+    filteredPerson = persons.filter((person) =>
+      person.name.toLowerCase().includes(inputName.toLowerCase())
+    );
+
+    setSearchResults(filteredPerson);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={filterName} onChange={handleFilter} />
+      </div>
+      <h2>Add a new</h2>
       <form method="post" onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleName} />
@@ -51,9 +80,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
+      {serchResults.map((person) => {
         return (
-          <div key={person.name}>
+          <div key={person.id}>
             {person.name} {person.number}
           </div>
         );
