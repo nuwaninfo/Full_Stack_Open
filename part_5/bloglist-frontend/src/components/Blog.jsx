325dc501ff7e8fd76d74968de82cbc3b3c3d3a03
Nuwan Fernando <nuwaninfo@gmail.com>
2024-02-26 13:22:57 +0200

    complete 5.7 Blog List Frontend, step 7

--- part_5/bloglist-frontend/src/components/Blog.jsx
+++ part_5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,51 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setdetailsVisible] = useState(false)
+
+  const hideWhenVisible = { display: detailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        &nbsp;
+        <button
+          style={hideWhenVisible}
+          type='button'
+          onClick={() => {
+            setdetailsVisible(true)
+          }}
+        >
+          view
+        </button>
+        <button
+          style={showWhenVisible}
+          type='button'
+          onClick={() => setdetailsVisible(false)}
+        >
+          hide
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <button type='button'>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
